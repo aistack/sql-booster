79c467c309c2eae2e266cf55d1f80d366ea24573
WilliamZhu <allwefantasy@gmail.com>
2019-07-19 14:08:02 +0800

    update

---
+++ src/main/java/tech/mlsql/sqlbooster/JavaDoc.java
@@ -0,0 +1,7 @@
+package tech.mlsql.sqlbooster;
+
+/**
+ * 2019-07-19 WilliamZhu(allwefantasy@gmail.com)
+ */
+public class JavaDoc {
+}
--- src/main/java/tech/mlsql/sqlbooster/JavaDoc.scala
+++
@@ -1,8 +0,0 @@
-package tech.mlsql.sqlbooster
-
-/**
-  * 2019-07-17 WilliamZhu(allwefantasy@gmail.com)
-  */
-class JavaDoc {
-
-}
